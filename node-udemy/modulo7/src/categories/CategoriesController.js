8ccb585f51754f19457bf6d33185d137c8883eed
Vinicius Justino <visilva5690@hotmail.com>
2023-07-29 17:52:18 -0300

    linkando o botão de edição com a rota

--- node-udemy/modulo7/src/categories/CategoriesController.js
+++ node-udemy/modulo7/src/categories/CategoriesController.js
@@ -8,7 +8,7 @@ router.get("/admin/categories/new", (req, res) => {
 })
 
 router.post("/categories/save", (req, res) => {
-    
+
     let title = req.body.title;
     if (title != undefined) {
 
@@ -16,7 +16,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)// transforma uma string em text fica ex: "Computação e informatica => computacao-e-informatica"
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         }).catch((msgErro) => {
             console.log(msgErro)
         })
@@ -29,7 +29,48 @@ router.post("/categories/save", (req, res) => {
 router.get("/admin/categories", (req, res) => {
 
     Category.findAll().then(categories => {
-        res.render("admin/categories/index", {categories: categories});
+        res.render("admin/categories/index", { categories: categories });
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+
+        } else { // se não for numero vai ser redirecionado
+            res.redirect("/admin/categories")
+        }
+    } else { // se for null vai ser redirecionado
+        res.redirect("/admin/categories")
+    }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id;
+
+    /*Validando se ele não for um número*/
+    /* if (isNan(id)) {
+        res.redirect("/admin/categories");
+    } */
+
+    Category.findByPk(id).then(category => { // pesquisando uma categoria pelo id
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+        console.log(erro);
     })
 })
 
