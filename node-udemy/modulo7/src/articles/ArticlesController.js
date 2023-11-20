9ffed0e82b0a492f61924062bd33da458265c082
Vinicius Justino <visilva5690@hotmail.com>
2023-11-20 16:29:02 -0300

    Formulário de artigos

--- node-udemy/modulo7/src/articles/ArticlesController.js
+++ node-udemy/modulo7/src/articles/ArticlesController.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get("/articles", (req, res) => {
     res.send("rotas de artigos")
-})
+});
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("rotas para criar um artigo")
-})
+    res.render('admin/articles/new')
+});
 
 module.exports = router;
