cc33d84769e8fdc6acf5c14107d291bf52541664
focaG <60098824+Edamatec@users.noreply.github.com>
2023-12-15 15:30:02 -0300

    feat: Instalacao do bcrypt + hash de senha

--- node-udemy/modulo7/src/user/UserController.js
+++ node-udemy/modulo7/src/user/UserController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
     res.send("listagem de usuários");
@@ -14,7 +15,18 @@ router.post("/users/create", (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    res.json({ email, password });
+    // primeiro criar um salt, temperar a senha
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt); // variavel para salvar a senha no banco
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/");
+    }).catch((err) => {
+        res.redirect("/");
+    })
 })
 
 module.exports = router;
