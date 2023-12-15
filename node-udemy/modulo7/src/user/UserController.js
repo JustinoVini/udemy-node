9a45c99d5a3383320d081ae5b01477a948043434
focaG <60098824+Edamatec@users.noreply.github.com>
2023-12-15 15:38:33 -0300

    feat: Validando email

--- node-udemy/modulo7/src/user/UserController.js
+++ node-udemy/modulo7/src/user/UserController.js
@@ -4,7 +4,10 @@ const User = require("./User");
 const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req, res) => {
-    res.send("listagem de usuários");
+
+    User.findAll().then(users => {
+        res.render("admin/users/index", { users: users });
+    })
 });
 
 router.get("/admin/users/create", (req, res) => {
@@ -15,17 +18,25 @@ router.post("/users/create", (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    // primeiro criar um salt, temperar a senha
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt); // variavel para salvar a senha no banco
+    User.findOne({
+        where: { email: email }
+    }).then(user => {
+        if (user == undefined) { // se não ter cadastrar
+            // primeiro criar um salt, temperar a senha
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt); // variavel para salvar a senha no banco
 
-    User.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect("/");
-    }).catch((err) => {
-        res.redirect("/");
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/admin/users");
+            }).catch((err) => {
+                res.redirect("/");
+            })
+        } else { // se ter faz nada
+            res.redirect("/admin/users/create");
+        }
     })
 })
 
