99c2bba074b48aee830695d7bf81772478ee0377
focaG <60098824+Edamatec@users.noreply.github.com>
2023-12-15 15:04:29 -0300

    feat: Criação do formulario de cadastro de usuario

--- node-udemy/modulo7/src/user/UserController.js
+++ node-udemy/modulo7/src/user/UserController.js
@@ -4,10 +4,17 @@ const User = require("./User");
 
 router.get("/admin/users", (req, res) => {
     res.send("listagem de usuários");
-})
+});
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
+});
+
+router.post("/users/create", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    res.json({ email, password });
 })
 
 module.exports = router;
