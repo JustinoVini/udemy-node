1e55f9d7f409236385351fb980e26f16c3af8b22
focaG <60098824+Edamatec@users.noreply.github.com>
2023-12-15 08:45:14 -0300

    feat: Limitando visualização na Home page/push do trabalho

--- node-udemy/modulo7/src/articles/ArticlesController.js
+++ node-udemy/modulo7/src/articles/ArticlesController.js
@@ -110,7 +110,10 @@ router.get("/articles/page/:num", (req, res) => {
 
     Article.findAndCountAll({
         limit: 4,
-        offset: offset
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
     }).then(articles => {
 
         let next;
