a35db0cf3ae940b252a12c535b74302d5e7f31a5
focaG <60098824+Edamatec@users.noreply.github.com>
2024-01-05 09:08:04 -0300

    Inserindo parametros em callbacks

--- node-udemy/modulo9/src/index.js
+++ node-udemy/modulo9/src/index.js
@@ -7,12 +7,19 @@ function enviarEmail(corpo, para, callback) {
         ----------------------------
         De: Vinicius
         `);
-        callback()
+        callback("OK", 5, "8s", "vinicius");
     }, 8000)
 }
 
 console.log("Inicio do envio de email")
-enviarEmail("Oi Seja bem vindo ao formação node", "vinicius@edamatec.com.br", () => {
-    console.log("Email enviado")
+enviarEmail("Oi Seja bem vindo ao formação node", "vinicius@edamatec.com.br", (status, amount, time, nome) => {
+    console.log(`
+    De: ${nome}
+    Status: ${status}
+    --------------------
+    Contatos: ${amount}
+    --------------------
+    Tempo de envio: ${time}
+    `)
     console.log("200")
 })
