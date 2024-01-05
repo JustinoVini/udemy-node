32ba42f3263454735af0be35535e5be1a747ff93
focaG <60098824+Edamatec@users.noreply.github.com>
2024-01-05 11:01:49 -0300

    Resolvendo desafio async await

---
+++ node-udemy/modulo9/src/resolvendoPromiseHell.js
@@ -0,0 +1,67 @@
+function pegarId() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5)
+        }, 2000)
+    })
+}
+
+function buscarIdNoBanco(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("teste@teste.com.br")
+        }, 3000)
+    })
+}
+
+function enviarEmail(corpo, para) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let deuErro = false;
+
+            if (!deuErro) { //promessa OK, pode ter somente 1 param, se quiser mais tem que ter {}
+                resolve({ time: 6, to: "Teste" })
+            } else {
+                reject("Fila cheia") // promessa deu erro
+            }
+
+        }, 4000)
+    });
+}
+
+function pegarUsuarios() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([
+                { name: "victor", lang: "java" },
+                { name: "just", lang: "java" },
+                { name: "daniel", lang: "aaa" }
+            ])
+        }, 4000)
+    })
+}
+
+async function principal() {
+    let id = await pegarId();
+    let email = await buscarIdNoBanco(id);
+    enviarEmail("olá", email).then(() => {
+        console.log("Email enviado")
+    }).catch((error) => {
+        console.log(error)
+    });
+}
+
+principal();
+
+
+
+/* pegarId().then((id) => {
+    buscarIdNoBanco(id).then((email) => {
+        enviarEmail("Olá como vai?", email).then(() => {
+            console.log("Email enviado, para o usuario com id: " + id)
+        }).catch((erro) => {
+            console.log(erro)
+        })
+    })
+}) */
